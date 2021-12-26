e8abc5412a8f7d10d0a387cea9d48cb5cf03f492
kevin <kevinteng2000@gmail.com>
2021-12-26 17:17:57 +0800

    update for max pdf size

--- src/Main.py
+++ src/Main.py
@@ -5,6 +5,7 @@ from PIL import Image
 from PDFtoJPG import convert
 import shutil
 from Config import config
+from PyPDF2 import PdfFileWriter,PdfFileReader,PdfFileMerger
 
 pdfdir, pdfname, problemMax = config()
 with open('../json/' + pdfname + '/structuredData.json', encoding="utf-8") as jsonfile:
@@ -23,11 +24,17 @@ def mkdir(path):
         os.makedirs(path)
 
 def CUT(page, problem, coopery1, coopery2):
+    pdf_file = PdfFileReader(open("../resources/" + pdfdir,"rb"))
+    mypage = pdf_file.getPage(0)
+    # print(mypage.cropBox.getUpperRight())
+    maxsizeX = int(mypage.cropBox.getUpperRight()[0])
+    maxsizeY = int(mypage.cropBox.getUpperRight()[1])
+    # print(maxsizeX, maxsizeY)
     img = Image.open("../CutOutput/" + pdfname + '/pdf/' + str(page) + '.jpg')
     # print(img.size)
-    d = img.size[1] / 842
-    y1 = 842 - coopery1
-    y2 = 842 - coopery2
+    d = img.size[1] / maxsizeY
+    y1 = maxsizeY - coopery1
+    y2 = maxsizeY - coopery2
     # print(y1, y2) # convert 後的座標(y軸)
     cropped = img.crop((0, y2 * d, img.size[0], y1 * d)) # crop((left, upper, right, lower))
     cutimg = "../CutOutput/" + pdfname + '/cut/' + str(problem - 1) + '.jpg'
@@ -41,7 +48,9 @@ Problem = 0
 mixdata = []
 mkdir("../CutOutput/" + pdfname + '/cut')
 for d in data['elements']:
-    try: 
+    try:
+        # "^[(][ ][)]\d{1,}[.]"
+        # "^\d{1,}[.][^0-9]" 
         if re.match(r"^\d{1,}[.][^0-9]", d['Text'] + "測試") is not None:
             Problem = Problem + 1   
             if Problem > problemMax:
@@ -71,6 +80,7 @@ f = open("../CutOutput/" + pdfname + '/output.txt', 'w', encoding="utf-8")
 f.writelines(output)
 f.close()
 
+# print(mixdata)
 for page, id, y1, y2 in mixdata:
     print('第%s頁 第%s題 y1 = %f y2 = %f' % (page + 1, id, y1, y2))
     CUT(page, id, y1, y2)
